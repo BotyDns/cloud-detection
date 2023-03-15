c5195294fe2b2bb99de21c35364b652cd5fdb3e7
BotyDns <61664283+BotyDns@users.noreply.github.com>
2023-03-15 01:06:24 +0100

    figure out the usage of gdal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use gdal::{Dataset, DriverManager};
+
+fn open_image(path: &str) {
+    let result = Dataset::open(path);
+    match result {
+        Ok(res) => println!("{} {}", res.raster_size().0, res.raster_size().1),
+        Err(err) => panic!("{}", err.to_string()),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    DriverManager::register_all();
+
+    open_image("E:/Programozas/terinfo/data/landsat_8-9/test/2023-02-10-00_00_2023-02-10-23_59_Landsat_8-9_L2_B05_(Raw).tiff");
 }
