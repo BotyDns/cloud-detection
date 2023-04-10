6872579524248ad82ccf6b36d0cd7f5d82352ee8
BotyDns <61664283+BotyDns@users.noreply.github.com>
2023-04-10 20:40:37 +0200

    move validate to util

--- src/classifiers/mcm/landsat/cloud.rs
+++ src/classifiers/mcm/landsat/cloud.rs
@@ -1,4 +1,4 @@
-use crate::classifiers::util::diff;
+use crate::classifiers::util;
 use crate::classifiers::Classification;
 
 use gdal::errors::GdalError;
@@ -20,38 +20,12 @@ impl Classifier {
         let reference_image = Dataset::open(reference_image_path)?;
         let target_image = Dataset::open(target_image_path)?;
 
-        Self::validate(&reference_image, &target_image)?;
+        util::validate(&reference_image, &target_image, MIN_RASTER_COUNT)?;
 
         Ok(Classifier {
             target: target_image,
             reference: reference_image,
         })
-    }
-
-    fn validate(reference_dataset: &Dataset, target_dataset: &Dataset) -> Result<(), GdalError> {
-        let reference_raster_count = reference_dataset.raster_count();
-        if reference_raster_count < MIN_RASTER_COUNT {
-            return Err(GdalError::BadArgument(format!("The reference image does not have enough bands!\nrequired:{MIN_RASTER_COUNT}\nactual:{reference_raster_count}")));
-        }
-
-        let target_raster_count = target_dataset.raster_count();
-        if target_raster_count < MIN_RASTER_COUNT {
-            return Err(GdalError::BadArgument(format!("The target image does not have enough bands!\nrequired:{MIN_RASTER_COUNT}\nactual:{target_raster_count}")));
-        }
-
-        let reference_raster_size = reference_dataset.raster_size();
-        let target_raster_size = target_dataset.raster_size();
-
-        if reference_raster_size != target_raster_size {
-            return Err(GdalError::BadArgument(format!("The size of the target image does not match the size of the reference image!\nreference image size:({},{})\ntarget image size:({},{})",
-                    reference_raster_size.0,
-                    reference_raster_size.1,
-                    target_raster_size.0,
-                    target_raster_size.1
-            )));
-        }
-
-        Ok(())
     }
 
     fn get_rasters(dataset: Dataset) -> Result<Vec<Buffer<f32>>, GdalError> {
@@ -67,7 +41,7 @@ impl Classification<u32> for Classifier {
         let target_rasters = Classifier::get_rasters(self.target)?;
 
         let deltas: Vec<Vec<f32>> = (2..6)
-            .map(|i| diff(&target_rasters[i].data, &reference_rasters[i].data))
+            .map(|i| util::diff(&target_rasters[i].data, &reference_rasters[i].data))
             .collect();
 
         let mask = deltas[0]
--- src/classifiers/util.rs
+++ src/classifiers/util.rs
@@ -1,3 +1,35 @@
+use gdal::{errors::GdalError, Dataset};
+
 pub fn diff(lhs: &Vec<f32>, rhs: &Vec<f32>) -> Vec<f32> {
     lhs.iter().zip(rhs).map(|(l, r)| l - r).collect()
+}
+
+pub fn validate(
+    reference_dataset: &Dataset,
+    target_dataset: &Dataset,
+    min_raster_count: isize,
+) -> Result<(), GdalError> {
+    let reference_raster_count = reference_dataset.raster_count();
+    if reference_raster_count < min_raster_count {
+        return Err(GdalError::BadArgument(format!("The reference image does not have enough bands!\nrequired:{min_raster_count}\nactual:{reference_raster_count}")));
+    }
+
+    let target_raster_count = target_dataset.raster_count();
+    if target_raster_count < min_raster_count {
+        return Err(GdalError::BadArgument(format!("The target image does not have enough bands!\nrequired:{min_raster_count}\nactual:{target_raster_count}")));
+    }
+
+    let reference_raster_size = reference_dataset.raster_size();
+    let target_raster_size = target_dataset.raster_size();
+
+    if reference_raster_size != target_raster_size {
+        return Err(GdalError::BadArgument(format!("The size of the target image does not match the size of the reference image!\nreference image size:({},{})\ntarget image size:({},{})",
+                reference_raster_size.0,
+                reference_raster_size.1,
+                target_raster_size.0,
+                target_raster_size.1
+        )));
+    }
+
+    Ok(())
 }
